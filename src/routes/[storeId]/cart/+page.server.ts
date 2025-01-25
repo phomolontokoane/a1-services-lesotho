282caca252913d99090cebe0c8d3cad2ec64c506
phomolontokoane <ntokoanephomolo@gmail.com>
2025-01-25 02:13:05 +0200

    start order system

--- src/routes/[storeId]/cart/+page.server.ts
+++ src/routes/[storeId]/cart/+page.server.ts
@@ -1,5 +1,45 @@
-import type { PageServerLoad } from './$types';
+import { supabase } from '$lib';
+import type { buyProduct } from '$lib/types';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async () => {
-    return {};
+	return {};
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	order: async ({ request }) => {
+		let formdata = await request.formData();
+		let price = formdata.get('total_price') as number | null;
+		let products = formdata.get('products') as string | null;
+		let owners = formdata.get('store') as string | null;
+		let user_id = formdata.get('user') as string | null;
+
+		if (!price || !products || !owners || !user_id) {
+			return { success: false, error: 'one of the fields is not filled' };
+		}
+
+		try {
+
+			// const { error: upError } = await supabase
+			// 	.from('Products')
+			// 	.update({ other_column: 'otherValue' })
+			// 	.eq('some_column', 'someValue')
+			// 	.select();
+
+			const { data, error } = await supabase
+				.from('Orders')
+				.insert([{ price, products: JSON.parse(products), owners, user_id: +user_id }])
+				.select();
+
+			if (error) {
+				console.error(error);
+				return { success: false, error: error.message };
+			}
+
+			return { success: true };
+		} catch (error) {
+			console.error(error);
+			return { success: false, error: 'Something went wrong' };
+		}
+	}
+};
--- src/routes/[storeId]/cart/+page.svelte
+++ src/routes/[storeId]/cart/+page.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
 	import { derived } from 'svelte/store';
-	import type { PageData } from './$types';
+	import type { ActionData, PageData } from './$types';
 	import { cart, clearStore } from '$lib/stores/cart';
 	import { store } from '$lib/stores/store';
 	import CartProduct from '$lib/components/CartProduct.svelte';
 	import { Button } from '$lib/components/ui/button';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-	let { data }: { data: PageData } = $props();
+	interface Props {
+		data: PageData;
+		form: ActionData;
+	}
+
+	let { data, form }: Props = $props();
+
+	onMount(() => {
+		if (form?.success) {
+			clearStore($store.owners);
+		}
+	});
+
 	let storeCart = derived(cart, ($cart) => $cart.filter((c) => c.owners == $store.owners));
 	let totalPrice = $state(0);
+	let products = $state('[]');
 
 	storeCart.subscribe((store) => {
 		let sum = 0;
@@ -16,6 +31,7 @@
 			sum += c.qty * c.price;
 		});
 		totalPrice = sum;
+		products = JSON.stringify(store);
 	});
 </script>
 
@@ -39,6 +55,12 @@
 			class="grow rounded text-xl"
 			onclick={() => clearStore($store.owners)}>Clear</Button
 		>
-		<Button class="grow rounded text-xl">Order</Button>
+		<form action="?/order" method="POST" class="grow">
+			<input type="hidden" name="total_price" bind:value={totalPrice} />
+			<input type="hidden" name="products" bind:value={products} />
+			<input type="hidden" name="store" value={$store.owners} />
+			<input type="hidden" name="user" value={data.user?.id} />
+			<Button class="w-full rounded text-xl" type="submit">Order</Button>
+		</form>
 	</div>
 </div>
