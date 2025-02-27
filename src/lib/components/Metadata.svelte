3d6e1501558368e9e1c900c754449e8ec8461048
Phomolo Ntokoane <ntokoanephomolo76@gmail.com>
2025-02-27 18:39:27 +0200

    Onesignal intergration

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -25,4 +25,17 @@
 	<meta property="twitter:image" content="https://a1-services-lesotho.vercel.app/favicon.png" />
 
 	<!-- Meta Tags Generated with https://metatags.io -->
+	<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+	<script>
+	  window.OneSignalDeferred = window.OneSignalDeferred || [];
+	  OneSignalDeferred.push(async function(OneSignal) {
+		await OneSignal.init({
+		  appId: "16aa67eb-16c8-4971-9406-aa21b826acde",
+		  safari_web_id: "web.onesignal.auto.6a13c381-7602-4e1d-b2a0-1098f769d9cc",
+		  notifyButton: {
+			enable: true,
+		  },
+		});
+	  });
+	</script>
 </svelte:head>
