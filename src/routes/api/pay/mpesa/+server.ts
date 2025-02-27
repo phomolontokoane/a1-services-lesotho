5a25d2aa367a24dbea9928e93450ec1a029e8d01
Phomolo Ntokoane <ntokoanephomolo76@gmail.com>
2025-02-27 18:42:14 +0200

    Pay methods update 1

--- src/routes/api/pay/mpesa/+server.ts
+++ src/routes/api/pay/mpesa/+server.ts
@@ -1,5 +1,12 @@
+import { text } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
+    return text("Hello /api/mpesa/pay");
+};
+
+export const POST: RequestHandler = async ({request}) => {
+    const data = await request.json();
+    console.log(data)
     return new Response();
 };
