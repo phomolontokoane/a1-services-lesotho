206e7435bba15c43b2a55ab1d5f65a7da1b8fafc
Phomolo Ntokoane <ntokoanephomolo76@gmail.com>
2025-03-15 12:28:31 +0200

    Implement notification permission request and update Navbar component to remove commented code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,33 @@
 	import Metadata from '$lib/components/Metadata.svelte';
 	import Navbar from '$lib/components/Navbar/Nav.svelte';
 	import type { LayoutData } from './$types';
-	import type { Snippet } from 'svelte';
+	import { onMount, type Snippet } from 'svelte';
 	import { user } from '$lib/stores/user';
+	import { toast } from 'svelte-sonner';
 
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 	if (data.user) user.set(data.user);
+
+	onMount(() => {
+		if (Notification.permission !== "granted") {
+			toast("Please allow notifications to receive updates", {
+				duration: 100000,
+				dismissable: true,
+				action: {
+					label: "Allow",
+					onClick: () => {
+						Notification.requestPermission().then((permission) => {
+							if (permission === "granted") {
+								toast("Notifications enabled");
+							} else {
+								toast("Notifications disabled");
+							}
+						});
+					},
+				},
+			});
+		}
+	})
 </script>
 
 <Toaster />
